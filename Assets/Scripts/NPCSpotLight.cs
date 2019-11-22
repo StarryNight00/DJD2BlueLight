fc43abb6b3dfc8d138c4bfb038a6136c64d3fd51
PmaiWoW <PmaiWoW@Gmail.com>
2019-11-22 17:33:54 +0000

    Add NPC test object and light source Spot Light to test lighting/shadows effect

---
+++ Assets/Scripts/NPCSpotLight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpotLight : MonoBehaviour
+{
+    [SerializeField]
+    private Light _spotlight;
+    private Transform _slTransf;
+    private float _slRange;
+    private IEnumerator slEffect;
+
+    // private Transform _spotlightTransform;
+
+    private void Awake()
+    {
+        _spotlight = GetComponent<Light>();
+        _slTransf = _spotlight.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slEffect = spotlightEffect(_spotlight, _slTransf, _slRange);
+        StartCoroutine(slEffect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator spotlightEffect(
+        Light sl, Transform slTransf, float range)
+    {
+        int counter = 0;
+        float secsUntilEffectStart = 1;
+        WaitForSeconds wfs = new WaitForSeconds(secsUntilEffectStart);
+
+        while (true)
+        {
+            counter++;
+            Debug.Log(counter);
+            yield return wfs;
+        }
+    }
+}
