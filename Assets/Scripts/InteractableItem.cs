94f9fee14f511c85259791d20e59a083ff30cbaf
StarryNight00 <cisa.matias@gmail.com>
2019-11-22 17:25:35 +0000

    Add InteractableItem class that defines a clickable object. Yet to add a textbox on mouse click.

---
+++ Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+    [SerializeField]    private string  objectName;
+    
+    public bool                 isInteractable;
+    private bool                isClicked;
+
+    private void OnMouseDown()
+    {
+        isClicked = true;
+    }
+
+    private void PrintName()
+    {
+        Debug.Log(objectName);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isInteractable)
+        {
+            if (isClicked)
+            {
+                Debug.Log(objectName);
+                Destroy(gameObject);
+            }
+
+            isClicked = false;
+        }
+    }
+}
