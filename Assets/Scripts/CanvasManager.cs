fd2a89c36a45d7e6be0f93e73391b99d35568279
StarryNight00 <cisa.matias@gmail.com>
2019-12-13 18:55:49 +0000

    Level design in first stages. Add player interaction and canvas classes.

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject interactionPanel;
+    public Text interactionText;
+
+    public void ShowInteractionPanel(string interactionMessage)
+    {
+        interactionText.text = interactionMessage;
+        interactionPanel.SetActive(true);
+    }
+    public void HideInteractionPanel()
+    {
+        interactionPanel.SetActive(false);
+    }
+
+    public void Start()
+    {
+
+        HideInteractionPanel();
+
+        //ShowInteractionPanel("dabadiiii");
+    }
+}
