a286566739492696828e467b69e8a654e9186486
AfonsoGR <afonso_rosa@hotmail.com>
2019-12-17 20:08:16 +0000

    Added scripts

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -7,22 +7,37 @@ public class CanvasManager : MonoBehaviour
 {
     public GameObject interactionPanel;
     public Text interactionText;
+    public Image[] inventoryIcons;
+
+    public void Start()
+    {
+        //ShowInteractionPanel("I am, in fact, working as expected");
+        HideInteractionPanel();
+    }
 
     public void ShowInteractionPanel(string interactionMessage)
     {
         interactionText.text = interactionMessage;
         interactionPanel.SetActive(true);
     }
+
     public void HideInteractionPanel()
     {
         interactionPanel.SetActive(false);
     }
 
-    public void Start()
+    public void SetInventoryIcon(int i, Sprite icon)
     {
+        inventoryIcons[i].sprite = icon;
+        inventoryIcons[i].color = Color.white;
+    }
 
-        HideInteractionPanel();
-
-        //ShowInteractionPanel("dabadiiii");
+    public void ClearInventoryIcons()
+    {
+        for (int i = 0; i < inventoryIcons.Length; ++i)
+        {
+            inventoryIcons[i].sprite = null;
+            inventoryIcons[i].color = Color.clear;
+        }
     }
 }
