29fd7557d6452575fc5aac5964ed86c571fa3bb1
AfonsoGR <afonso_rosa@hotmail.com>
2020-01-15 18:18:22 +0000

    Initial Main and Pause Menu done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource soundSource;
+    public AudioClip ding;
+    public AudioClip dong;
+
+    public void PlayGame()
+    {
+        soundSource.clip = dong;
+        soundSource.Play();
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        soundSource.clip = ding;
+        soundSource.Play();
+        Application.Quit();
+    }
+
+    public void Logo()
+    {
+        soundSource.clip = dong;
+        soundSource.Play();
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool IsPaused;
+
+    public GameObject Pause;
+
+    public AudioSource soundSource;
+    public AudioClip ding;
+    public AudioClip dong;
+
+    public void ExitToMainMenu()
+    {
+        soundSource.clip = dong;
+        soundSource.Play();
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitToDesktop()
+    {
+        soundSource.clip = ding;
+        soundSource.Play();
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (IsPaused)
+        {
+            Pause.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Pause.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPaused = !IsPaused;
+        }
+    }
+}
