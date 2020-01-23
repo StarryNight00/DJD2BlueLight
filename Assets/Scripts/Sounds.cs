9ec105af9d26fecebaa381623d49ff22b95fc926
AfonsoGR <afonso_rosa@hotmail.com>
2020-01-23 19:52:41 +0000

    AlmostDone, level fully setup

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioSource soundSource;
+    public AudioClip steps;
+    public AudioClip door;
+
+    CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        StepsSound();
+    }
+
+    private void StepsSound()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && soundSource.isPlaying == false)
+        {
+            soundSource.clip = steps;
+            soundSource.volume = Random.Range(0.4f, 6f);
+            soundSource.pitch = Random.Range(0.7f, 1f);
+            soundSource.Play();
+        }
+    }
+
+    public void DoorSound()
+    {
+        soundSource.clip = door;
+        soundSource.Play();
+    }
+}
